a17e6f21fdae3eeafb139645b612c606a721e017
Chad Montoya <chadmontoya21@yahoo.com>
2018-01-31 03:30:19 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,6 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
 
   def change
     create_table :costumes do |t|
-      binding.pry
       t.string :name
       t.integer :price
       t.text :image_url
